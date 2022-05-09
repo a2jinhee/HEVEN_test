81c2a2472cb218cd820e14bb4e4d56c21c570847
a2jinhee <a2jinhee@gmail.com>
2022-05-10 02:05:30 +0900

    openPath findPath

--- HEVEN_test/소스.c
+++ HEVEN_test/소스.c
@@ -3,14 +3,39 @@
 #include<math.h>
 #define _CRT_SECURE_NO_WARNINGS
 
+int goal[50][50]; int xgoal, ygoal;
+
+int openPath(int* n, int xcar, int ycar) {
+
+	if (n[xcar, ycar] != 0) {
+		return 0;
+	}
+	return 1; 
+}
+
+int findPath(int *diffPath) {
+	int min = 0;
+	for (int i = 0; i < 4; i++) {
+		if (diffPath[i] == NULL) {
+			continue;
+		}
+		if (diffPath[i] < diffPath[min]){
+			min = i;
+		}
+	}
+	return min;
+}
+
+
 int main() {
 
 	int N, a, b;
 	int n[50][50];
 	int open[50][50]; 
 	int car[50][50]; int xcar, ycar;
-	int goal[50][50]; int xgoal, ygoal;
 	int wall[50][50];
+	int diffPath[4];
+	int cost = 0;
 
 
 	scanf("%d", &N);
@@ -21,7 +46,7 @@ int main() {
 			for (int j = 0; j < b; j++) {
 				scanf("%d", &n[a][b]);
 
-				switch (n[a][b]) {
+ 				switch (n[a][b]) {
 				case 0: open[a][b] = 1; break;
 				case 1: car[a][b] = 1;
 					xcar = a, ycar = b; break;
@@ -32,10 +57,49 @@ int main() {
 				}
 			}
 		}
+
+		while ((xcar == xgoal) && (ycar == ygoal)) {
+
+			if (openPath(n, xcar + 1, ycar)) {
+				diffPath[0] = abs(xgoal - (xcar + 1)) + abs(ygoal - ycar);
+			}
+			else {
+				diffPath[0] = NULL; 
+			}
+			if (openPath(n, xcar - 1, ycar)) {
+				diffPath[1] = abs(xgoal - (xcar - 1)) + abs(ygoal - ycar);
+			}
+			else {
+				diffPath[1] = NULL;
+			}
+			if (openPath(n, xcar, ycar + 1)) {
+				diffPath[2] = abs(xgoal - xcar) + abs(ygoal - (ycar + 1));
+			}
+			else {
+				diffPath[2] = NULL;
+			}
+			if (openPath(n, xcar, ycar - 1)) {
+				diffPath[3] = abs(xgoal - xcar) + abs(ygoal - (ycar - 1));
+			}
+			else {
+				diffPath[3] = NULL;
+			}
+
+			switch (findPath(diffPath)) {
+			case 0: xcar = xcar + 1; break;
+			case 1: xcar = xcar - 1; break;
+			case 2: ycar = ycar + 1; break;
+			case 3: ycar = ycar - 1; break;
+			}
+
+			cost++;
+		}
+
+		printf("%d", cost);
 		printf("\n");
+
 	}
 	
-
 	return 0;
 
 }
